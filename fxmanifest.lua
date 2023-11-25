ea75b8ad6b5819b3176f2d85485490a9e2422133
FTF Development <149843153+Trackadev@users.noreply.github.com>
2023-11-25 16:02:02 -0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+auhtor 'tracka'
+dsescripton 'easy to use trucker job'
+---------------
+
+server_scripts {
+    '@es_extended/locale.lua',
+	'locales/de.lua',
+	'locales/en.lua',
+    'locales/fr.lua',
+	'config.lua',
+	'server/server.lua'
+}
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/de.lua',
+	'locales/en.lua',
+    'locales/fr.lua',
+	'config.lua',
+	'client/client.lua'
+}
